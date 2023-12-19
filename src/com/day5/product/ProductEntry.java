3cb5c75ce2529619b33e84f3d9397ed5ac23db44
Avinash <Avinash@ZML-LP70>
2023-12-19 22:30:48 +0530

    commplete

--- src/com/day5/product/ProductEntry.java
+++ src/com/day5/product/ProductEntry.java
@@ -7,41 +7,42 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class ProductEntry {
+public class ProductEntry 
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("How many products to be saved: ");
         int n = sc.nextInt();
 
-        // List to store products
         List<Product> productList = new ArrayList<>();
 
-        // Enter product details
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) 
+        {
             System.out.print("Enter product id & product name of Product-" + i + ": ");
             int productId = sc.nextInt();
-            sc.nextLine(); // Consume the newline character
+            sc.nextLine(); 
             String productName = sc.nextLine();
 
-            // Create a new Product object
             Product product = new Product(productId, productName);
 
-            // Add the product to the list
             productList.add(product);
         }
 
-        // Save product details to file using serialization
         saveProductsToFile(productList);
 
         System.out.println("Product details saved to stocks.ser");
     }
 
-    private static void saveProductsToFile(List<Product> productList) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("stocks.ser"))) {
+    private static void saveProductsToFile(List<Product> productList) 
+    {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("stocks.ser"))) 
+        {
             oos.writeObject(productList);
-        } catch (IOException e) {
+        } catch (IOException e)
+        {
             e.printStackTrace();
         }
     }
--- src/com/day5/product/ProductFetch.java
+++ src/com/day5/product/ProductFetch.java
@@ -7,49 +7,59 @@ import java.io.ObjectInputStream;
 import java.util.List;
 import java.util.Scanner;
 
-public class ProductFetch {
+public class ProductFetch 
+{
 
-    public static void main(String[] args) {
-        // Fetch product based on Product ID
+    public static void main(String[] args) 
+    {
+       
         fetchProduct();
     }
 
-    private static void fetchProduct() {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("stocks.ser"))) {
+    private static void fetchProduct() 
+    {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("stocks.ser")))
+        {
             // Read the list of products from the file
             List<Product> productList = (List<Product>) ois.readObject();
            Product p = new Product(1, "b");
             productList.add(p);
 
-            if (productList.isEmpty()) {
+            if (productList.isEmpty()) 
+            {
                 System.out.println("Please enter Stock Details.");
                 return;
             }
 
-            // Get input for product ID
             Scanner scanner = new Scanner(System.in);
             System.out.print("Enter product ID: ");
             int targetProductId = scanner.nextInt();
-scanner.nextLine();
-            // Find the product in the list
+            scanner.nextLine();
+           
             Product foundProduct = null;
-            for (Product product : productList) {
-                if (product.getProductId() == targetProductId) {
+            for (Product product : productList)
+            {
+                if (product.getProductId() == targetProductId) 
+                {
                     foundProduct = product;
-                   // break;
+                 
                 }
             }
 
-            // Display result
-            if (foundProduct != null) {
+            if (foundProduct != null) 
+            {
                 System.out.println("Product Name: " + foundProduct.getProductName());
-            } else {
+            } else 
+            {
                 System.out.println("Product not found. Please enter a valid product ID.");
             }
 
-        } catch (FileNotFoundException e) {
+        } 
+        catch (FileNotFoundException e)
+        {
             System.out.println("Please enter Stock Details.");
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException e) 
+        {
             e.printStackTrace();
         }
     }
